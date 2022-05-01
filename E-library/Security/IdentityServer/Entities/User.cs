5f37be8f5eeb3afa0417ede35507df3f4ee4b508
josicb16 <josicbojana16@gmail.com>
2022-05-01 23:23:17 +0200

    Napravljeni entiteti User i Role u IdentityServer-u

---
+++ E-library/Security/IdentityServer/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        // TO DO: korisnik moze da se registruje putem imejla ili mobilnog telefona (mozda i fb)
+        // TO DO: dodati polje koje oznacava datum kada se korisnik uclanio u biblioteku
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string userName) : base(userName)
+        {
+        }
+    }
+}

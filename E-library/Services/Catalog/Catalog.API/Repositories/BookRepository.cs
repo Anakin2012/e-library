088d334692905ffc7f088d53f5f4713b8197f7b6
panapetrovic <pana.petrovic@gmail.com>
2022-05-02 13:19:36 +0200

    Dodat repozitorijum za knjige sa metodom GetBook

---
+++ E-library/Services/Catalog/Catalog.API/Repositories/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public BookRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _context.Books.Find(b => true).ToListAsync(); // asinhrono pretvara kolekciju u listu
+        }
+    }
+
+    
+}
---
+++ E-library/Services/Catalog/Catalog.API/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetBooks();
+    }
+}

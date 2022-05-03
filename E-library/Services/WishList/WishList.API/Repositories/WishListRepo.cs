46e4a64b68ef452e55355d39a78e9348dbfe471b
altgmbobo <bbbole999@gmail.com>
2022-05-03 22:47:32 +0200

    WishList Repo + Entities

---
+++ E-library/Services/WishList/WishList.API/Repositories/WishListRepo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Threading.Tasks;
+using WishList.API.Entities;
+
+namespace WishList.API.Repositories
+{
+    public class WishListRepo : IWishListRepo
+    {
+        private readonly IDistributedCache _cache;
+        
+        Task<WishBookList> IWishListRepo.GetList(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<WishBookList> UpdateList(WishBookList list)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        Task IWishListRepo.DeleteList(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

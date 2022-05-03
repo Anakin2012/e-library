d2e9cb2cd48150f95c7686284ff852ef3ee8d29d
josicb16 <josicbojana16@gmail.com>
2022-05-03 16:58:24 +0200

    Napravljena klasa RegistrationController

---
+++ E-library/Security/IdentityServer/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using IdentityServer.DTOs;
+using IdentityServer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+
+        private readonly UserManager<Member> _memberManager;
+        private readonly RoleManager<Role> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RegistrationController(UserManager<Member> memberManager, RoleManager<Role> roleManager, IMapper mapper)
+        {
+            _memberManager = memberManager ?? throw new ArgumentNullException(nameof(memberManager));
+            _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+
+    }
+}

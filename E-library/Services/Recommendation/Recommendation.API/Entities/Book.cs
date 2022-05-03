981963c0df0175c326a69b3d1ebda55b654939b9
Luka Djorovic <luka.djorovic3010@gmail.com>
2022-05-03 14:44:07 +0200

    Dodati entiteti za mikroservis za preporuku

---
+++ E-library/Services/Recommendation/Recommendation.API/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recommendation.API.Entities
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string CoverImageFile { get; set; }
+
+    }
+}
---
+++ E-library/Services/Recommendation/Recommendation.API/Entities/RecommendationList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recommendation.API.Entities
+{
+    public class Recommendation
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public List<Book> Recommendations { get; set; } = new List<Book>();
+    }
+}
+

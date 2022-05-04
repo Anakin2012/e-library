910efd2b3ac4f38017f38e9ee1a80f9f21e4af60
panapetrovic <pana.petrovic@gmail.com>
2022-05-04 20:23:03 +0200

    fix konflikta oko generisanih fajlova

--- E-library/Services/Catalog/Catalog.API/Repositories/BookRepository.cs
+++ E-library/Services/Catalog/Catalog.API/Repositories/BookRepository.cs
@@ -22,6 +22,31 @@ namespace Catalog.API.Repositories
         {
             return await _context.Books.Find(b => true).ToListAsync(); // asinhrono pretvara kolekciju u listu
         }
+        public async Task<Book> GetBook(string id)
+        {
+            return await _context.Books.Find(b => b.Id == id).FirstOrDefaultAsync(); // vraca prvi ili podrazumevanu vrednost
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksByGenre(string genre)
+        {
+            return await _context.Books.Find(b => b.Genre == genre).ToListAsync();
+        }
+
+        public async Task CreateBook(Book book)
+        {
+            await _context.Books.InsertOneAsync(book);
+        }
+
+        public async Task<bool> UpdateBook(Book book)
+        {
+            var updateResult = await _context.Books.ReplaceOneAsync(b => b.Id == book.Id, book);
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+        public async Task<bool> DeleteBook(string id)
+        {
+            var deleteResult = await _context.Books.DeleteOneAsync(b => b.Id == id);
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
     }
 
     
--- E-library/Services/Catalog/Catalog.API/Repositories/Interfaces/IBookRepository.cs
+++ E-library/Services/Catalog/Catalog.API/Repositories/Interfaces/IBookRepository.cs
@@ -9,5 +9,10 @@ namespace Catalog.API.Repositories.Interfaces
     public interface IBookRepository
     {
         Task<IEnumerable<Book>> GetBooks();
+        Task<Book> GetBook(string id);
+        Task<IEnumerable<Book>> GetBooksByGenre(string genre);
+        Task CreateBook(Book book);
+        Task<bool> UpdateBook(Book book);
+        Task<bool> DeleteBook(string id);
     }
 }

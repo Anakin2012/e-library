963f8d59f6932223af0571d92ffb4092ca1762e2
altgmbobo <bbbole999@gmail.com>
2022-07-20 22:10:04 +0200

    Persistance + Repos + Factories

---
+++ Ordering.Infrastructure/Factories/OrderFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Factories
+{
+    public class OrderFactory : IOrderFactory
+    {
+        public Order Create(CreateOrderCommand command) {
+            var order = new Order(command.BuyerId, command.BuyerUsername, new Address(command.Street, command.City, command.State, command.Country, command.ZipCode, command.EmailAddress));
+            foreach(var item in command.OrderItems)
+            {
+                order.AddOrderItem(item.ProductName, item.ProductId, item.PictureUrl, item.Price, item.Units);
+
+            }
+            return order;
+        }
+
+
+    }
+}
---
+++ Ordering.Infrastructure/Persistance/OrderContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Domain.Aggregates;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seeding database associated with context {DbContextName}", nameof(OrderContext));
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            var order1 = new Order("1", "rs2", new Address("Ulica 1", "Beograd", "Beograd", "Srbija", "11000", "razvoj.softvera.matf@gmail.com"));
+            order1.AddOrderItem("IPhone X", "602d2149e773f2a3990b47f5", "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-x-new-1.jpg", 800, 2);
+            order1.AddOrderItem("HTC U11+ Plus", "602d2149e773f2a3990b47f9", "https://fdn2.gsmarena.com/vv/pics/htc/htc-u11-plus-1.jpg", 380, 1);
+
+            return new List<Order> { order1 };
+        }
+    }
+}

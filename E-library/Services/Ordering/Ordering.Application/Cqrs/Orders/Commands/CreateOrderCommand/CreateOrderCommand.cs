9e748edbe8a2ab895a17a4aa634d31736650211f
Luka Djorovic <luka.djorovic3010@gmail.com>
2022-07-20 20:07:59 +0200

    Implementacija Ordering.Application projekta (Bez behavior-a - check point)

---
+++ E-library/Services/Ordering/Ordering.Application/Cqrs/Orders/Commands/CreateOrderCommand/CreateOrderCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Ordering.Application.Cqrs.Orders.Commands.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Cqrs.Orders.Commands.CreateOrderCommand
+{
+    public class CreateOrderCommand : IRequest<int>
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public string EmailAddress { get; set; }
+
+
+        public string CustomerId { get; set; }
+        public string Username { get; set; }
+        public IEnumerable<OrderItemDTO> OrderItems { get; set; }
+
+    }
+}
---
+++ E-library/Services/Ordering/Ordering.Application/Cqrs/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Ordering.Application.Cqrs.Orders.Queries.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Cqrs.Orders.Queries.GetOrders
+{
+    public class GetOrdersQuery : IRequest<List<OrderDTO>>
+    {
+
+        public string Username { get; set; }
+
+        public GetOrdersQuery(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+    }
+}

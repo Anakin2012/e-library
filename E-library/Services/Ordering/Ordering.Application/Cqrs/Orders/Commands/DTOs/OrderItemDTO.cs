74d21f36d6ecfa7db7d6b5bd1a8d2f525435dae5
altgmbobo <bbbole999@gmail.com>
2022-07-22 00:17:43 +0200

    abc

---
+++ E-library/Services/Ordering/Ordering.Application/Cqrs/Orders/Commands/DTOs/OrderItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Cqrs.Orders.Commands.DTOs
+{
+    public class OrderItemDTO
+    {
+        public string BookTitle { get; set; }
+        public string BookId { get; set; }
+    }
+}
---
+++ E-library/Services/Ordering/Ordering.Application/Cqrs/Orders/Queries/DTOs/OrderDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Cqrs.Orders.Queries.DTOs
+{
+    public class OrderDTO
+    {
+
+        public int Id { get; set; }
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public string EmailAddress { get; set; }
+
+        // Relevant information from Order
+        public string CustomerId { get; set; }
+        public string Username { get; set; }
+        public IEnumerable<OrderItemDTO> OrderItems { get; set; }
+    }
+}

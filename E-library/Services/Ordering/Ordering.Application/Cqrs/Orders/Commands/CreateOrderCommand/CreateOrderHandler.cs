a0ddf29be901c6551393acdac17de92c375f581a
Luka Djorovic <luka.djorovic3010@gmail.com>
2022-07-22 21:15:54 +0200

    Kommitovan Ordering.Application nazad

---
+++ E-library/Services/Ordering/Ordering.Application/Cqrs/Orders/Commands/CreateOrderCommand/CreateOrderHandler.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Factories;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Cqrs.Orders.Commands.CreateOrderCommand
+{
+    public class CreateOrderHandler : IRequestHandler<CreateOrderCommand, int>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IOrderFactory _factory;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<CreateOrderHandler> _logger;
+
+        public CreateOrderHandler(IOrderRepository repository, IOrderFactory factory, IEmailService emailService, ILogger<CreateOrderHandler> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<int> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _factory.Create(request);
+            var newOrder = await _repository.AddAsync(orderEntity);
+
+            _logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+
+            await SendMail(newOrder);
+
+            return newOrder.Id;
+        }
+
+        private async Task SendMail(Order newOrder)
+        {
+            var email = new Email
+            {
+                Receiver = newOrder.Address.EmailAddress,
+                Title = $"Order {newOrder.Id} is sent",
+                Body = "You have placed a new order on Webstore."
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+                _logger.LogInformation($"Email successfully sent.");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Sending email for order {newOrder.Id} failed due to error: {e.Message}");
+            }
+        }
+    }
+}
---
+++ E-library/Services/Ordering/Ordering.Application/Cqrs/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Ordering.Application.Contracts.Factories;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Cqrs.Orders.Queries.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Cqrs.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler : IRequestHandler<GetOrdersQuery, List<OrderDTO>>
+    {
+
+        private readonly IOrderRepository _repository;
+        private readonly IOrderDTOFactory _factory;
+        public async Task<List<OrderDTO>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _repository.GetOrdersByUsername(request.Username);
+            return orderList.Select(order => _factory.CreateOrderDTO(order)).ToList();
+        }
+    }
+}

2bd35a347e42a7198b09c1ada9064fae4f0b0884
josicb16 <95657159+josicb16@users.noreply.github.com>
2022-08-08 13:23:17 +0200

    Ispravka u ispisu greske u metodu RegisterMember

--- E-library/Security/IdentityServer/Repositories/IdentityRepository.cs
+++ E-library/Security/IdentityServer/Repositories/IdentityRepository.cs
@@ -4,6 +4,7 @@ using IdentityServer.Entities;
 using IdentityServer.Repositories.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,28 +18,29 @@ namespace IdentityServer.Repositories
         private readonly UserManager<Member> _memberManager;
         private readonly RoleManager<Role> _roleManager;
         private readonly IMapper _mapper;
+        private readonly ILogger<IdentityRepository> _logger;
 
-
-        public IdentityRepository(UserManager<Member> memberManager, RoleManager<Role> roleManager, IMapper mapper)
+        public IdentityRepository(UserManager<Member> memberManager, RoleManager<Role> roleManager, IMapper mapper, ILogger<IdentityRepository> logger)
         {
             _memberManager = memberManager ?? throw new ArgumentNullException(nameof(memberManager));
             _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-
-
 
         private async Task<IdentityResult> RegisterEmail(NewMemberDTO newMember, IEnumerable<string> roles)
         {
             var member = _mapper.Map<Member>(newMember);
 
-            var memberUserName = _memberManager.FindByNameAsync(member.UserName);
-            if (memberUserName != null) {
-                IdentityError[] errors = new IdentityError[] { };
-                errors.Append(new IdentityErrorDescriber().DuplicateUserName(member.UserName));
 
-                return IdentityResult.Failed(errors);
+            var memberUserName = await _memberManager.FindByNameAsync(member.UserName);
+            if (memberUserName != null) {
+                IEnumerable<IdentityError> errors = Enumerable.Empty<IdentityError>();
+                errors = errors.Append(new IdentityErrorDescriber().DuplicateUserName(member.UserName));
+
+                return IdentityResult.Failed(errors.ToArray());
             }
+
 
             IdentityResult result = await _memberManager.CreateAsync(member, newMember.Password);
             if (!result.Succeeded)
@@ -61,12 +63,14 @@ namespace IdentityServer.Repositories
 
         public async Task<IdentityResult> RegisterAdministratorEmail(NewMemberDTO newMember)
         {
+
             return await RegisterEmail(newMember, new string[] { "Administrator" });
         }
 
 
         public async Task<IdentityResult> RegisterMemberEmail(NewMemberDTO newMember)
         {
+
             return await RegisterEmail(newMember, new string[] { "Member" });
         }
 
@@ -114,10 +118,11 @@ namespace IdentityServer.Repositories
             var member = await _memberManager.FindByNameAsync(username);
             if (member == null)
             {
-                IdentityError[] errors = new IdentityError[] { };
-                errors.Append(new IdentityErrorDescriber().InvalidUserName(username));
 
-                return IdentityResult.Failed(errors);
+                IEnumerable<IdentityError> errors = Enumerable.Empty<IdentityError>();
+                errors = errors.Append(new IdentityErrorDescriber().InvalidUserName(username));
+
+                return IdentityResult.Failed(errors.ToArray());
 
             }
             else
@@ -133,10 +138,12 @@ namespace IdentityServer.Repositories
             var exists_new_username = await _memberManager.FindByNameAsync(newUsername);
 
             if (exists_new_username != null) {
-                IdentityError[] errors = new IdentityError[] { };
-                errors.Append(new IdentityErrorDescriber().DuplicateUserName(newUsername));
 
-                return IdentityResult.Failed(errors);
+
+                IEnumerable<IdentityError> errors = Enumerable.Empty<IdentityError>();
+                errors = errors.Append(new IdentityErrorDescriber().DuplicateUserName(newUsername));
+
+                return IdentityResult.Failed(errors.ToArray());
             }
 
             Member member = await _memberManager.FindByNameAsync(currentUsername);

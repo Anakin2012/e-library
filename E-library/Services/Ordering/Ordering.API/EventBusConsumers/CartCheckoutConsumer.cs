e038690bb7deb2c04913fcdb8cac8474f0f59168
Ana Petrovic <pana.petrovic@gmail.com>
2022-08-09 19:05:27 +0200

    Komunikacija izmedju korpe i ordering.api
    zavrsena podesavanja za redove poruka. ostalo jos u docker-compose da se popuni za ordering.api
    closes #128

---
+++ E-library/Services/Ordering/Ordering.API/EventBusConsumers/CartCheckoutConsumer.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Cqrs.Orders.Commands.CreateOrderCommand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumers
+{
+    public class CartCheckoutConsumer : IConsumer<CartCheckoutEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CartCheckoutConsumer> _logger;
+
+        public CartCheckoutConsumer(IMediator mediator, IMapper mapper, ILogger<CartCheckoutConsumer> logger)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Consume(ConsumeContext<CartCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CreateOrderCommand>(context.Message);
+            var id = await _mediator.Send(command);
+
+            _logger.LogInformation($"{typeof(CartCheckoutEvent).Name} consumed successfully. Created order id: {id}");
+
+        }
+    }
+}
---
+++ E-library/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ordering.Application.Cqrs.Orders.Commands.CreateOrderCommand;
+using Ordering.Application.Cqrs.Orders.Commands.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Mapper
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CreateOrderCommand, EventBus.Messages.Events.CartCheckoutEvent>().ReverseMap();
+            CreateMap<OrderItemDTO, EventBus.Messages.Events.CartItem>().ReverseMap();
+        }
+    }
+}

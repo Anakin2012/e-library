45eea63b3bc6ec587d2b15ecad85be84ca36fc08
Ana Petrovic <pana.petrovic@gmail.com>
2022-08-12 20:47:06 +0200

    zapocet razvoj angular aplikacije
    Template izgleda stranice

--- E-library/WebApp/LibrarySPA/src/app/app-routing.module.ts
+++ E-library/WebApp/LibrarySPA/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from './basket/basket.component';
+import { BookComponent } from './book/book.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ContainerComponent } from './Container/container.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const appRoute: Routes = [
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+  {path: 'Home', component: HomepageComponent},
+  {path: 'Books', component: CatalogComponent}, // catalog
+  {path: 'Basket', component: BasketComponent},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoute)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- E-library/WebApp/LibrarySPA/src/app/app.module.ts
+++ E-library/WebApp/LibrarySPA/src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BasketComponent } from './basket/basket.component';
+
+
+  //  {path: 'Cart', component: CartComponent}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent,
+    SearchComponent,
+    BooksComponent,
+    BookComponent,
+    CatalogComponent,
+    HomepageComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

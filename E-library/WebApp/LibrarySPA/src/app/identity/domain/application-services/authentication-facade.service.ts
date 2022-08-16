9ccd0de19663effb1941e88f5cfff8dac8ac4dce
josicb16 <95657159+josicb16@users.noreply.github.com>
2022-08-16 18:20:55 +0200

    Dodato stanje i omoguceno skladistenje stanja

--- E-library/WebApp/LibrarySPA/src/app/identity/domain/application-services/authentication-facade.service.ts
+++ E-library/WebApp/LibrarySPA/src/app/identity/domain/application-services/authentication-facade.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
+import { AppStateService } from 'src/app/shared/app-state/app-state.service';
 import { AuthenticationService } from '../infrastructure/authentication.service';
 import { ILoginRequest } from '../models/login-request';
 import { ILoginResponse } from '../models/login-response';
@@ -9,14 +10,15 @@ import { ILoginResponse } from '../models/login-response';
 })
 export class AuthenticationFacadeService {
 
-  constructor(private authenticationService: AuthenticationService) {  }
+  constructor(private authenticationService: AuthenticationService, private appStateService : AppStateService) {  }
 
   public Login(loginName: string, password : string) : Observable<boolean> {
     const request : ILoginRequest = {loginName, password};
 
     return this.authenticationService.Login(request).pipe(
       map((loginResponse : ILoginResponse) => {
-        console.log(loginResponse);
+        this.appStateService.setAccessToken(loginResponse.accessToken);
+        this.appStateService.setRefreshToken(loginResponse.refreshToken);
         return true;
       }),
       catchError((err) => {

4ce9ed86b3f5c34bda9986419b91b7a29be98ab3
josicb16 <95657159+josicb16@users.noreply.github.com>
2022-08-16 15:49:24 +0200

    Dodat servis za registraciju

--- E-library/WebApp/LibrarySPA/src/app/identity/feature-authorisation/register-form/register-form.component.ts
+++ E-library/WebApp/LibrarySPA/src/app/identity/feature-authorisation/register-form/register-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthorisationFacadeService } from '../../domain/application-services/authorisation-facade.service';
+
 
 @Component({
   selector: 'app-register-form',
@@ -9,14 +11,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegisterFormComponent implements OnInit {
   public registerForm: FormGroup;
 
-  constructor() {
+  constructor(private authorisationService: AuthorisationFacadeService) {
     this.registerForm = new FormGroup({
       name: new FormControl("", [Validators.required]),
       surname: new FormControl("", [Validators.required]),
       username: new FormControl("", [Validators.required]),
-      password: new FormControl("", [Validators.required, Validators.minLength(8)]),
       email: new FormControl("", [Validators.required, Validators.email]),
       credentials: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)]),
       membership: new FormControl("", [Validators.required])
     });
    }
@@ -25,7 +27,35 @@ export class RegisterFormComponent implements OnInit {
   }
 
   public onRegisterFormSubmit() : void {
-    // TODO
+    if (this.registerForm.invalid) {
+      window.alert('Form is invalid');
+      return;
+    }
+
+
+    if(this.registerForm.value.membership === 'regular') {
+      this.authorisationService.RegisterMember(this.registerForm.value.name, this.registerForm.value.surname, this.registerForm.value.username, this.registerForm.value.password, this.registerForm.value.email, parseFloat(this.registerForm.value.credentials)).subscribe((success: boolean) => {
+        if(success==true) {
+          window.alert('Account created successfuly!');
+        }
+        else {
+          window.alert('Creation of the account failed!');
+        }
+        this.registerForm.reset();
+      });
+  }
+  if(this.registerForm.value.membership === 'premium') {
+    this.authorisationService.RegisterPremiumMember(this.registerForm.value.name, this.registerForm.value.surname, this.registerForm.value.username, this.registerForm.value.password, this.registerForm.value.email, parseFloat(this.registerForm.value.credentials)).subscribe((success: boolean) => {
+      if(success==true) {
+        window.alert('Account created successfuly!');
+      }
+      else {
+        window.alert('Creation of the account failed!');
+      }
+      this.registerForm.reset();
+    });
+  }
+
   }
 
 }

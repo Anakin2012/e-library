ae9029a338d68ccf45e10ba1a7a75a6aae600773
Ana Petrovic <pana.petrovic@gmail.com>
2022-08-16 16:21:53 +0200

    Implementirano azuriranje knjige
    U admin rezimu, implementiran PUT zahtev za updateBook

--- E-library/WebApp/LibrarySPA/src/app/catalog/feature-admin/admin-options/admin-options.component.ts
+++ E-library/WebApp/LibrarySPA/src/app/catalog/feature-admin/admin-options/admin-options.component.ts
@@ -12,7 +12,9 @@ import { IBook } from '../../domain/models/book';
 export class AdminOptionsComponent implements OnInit {
 
   allBooks: IBook[] = [];
+  editMode: boolean = false;
   @ViewChild('booksForm') form: NgForm;
+  currentBookId: string;
 
   constructor(private service: BooksFacadeService) { }
 
@@ -26,23 +28,33 @@ export class AdminOptionsComponent implements OnInit {
 
   onBookCreate(books: {title: string, author: string, genre: string, language:string, description: string, 
                       coverImageFile: string, isAvailable: boolean, isPremium: boolean, rentCount: number, id: string}) {
-      if (books.isPremium != true){
-        books.isPremium = false;
-      }
-      if (books.isAvailable != true) {
-        books.isAvailable = false;
-      }
-      
-      console.log(books);
-      this.service.createBook(books).subscribe((res) => {
-        console.log(res);
-      });
+    if (!this.editMode) {
+      this.createBook(books);
+    }
+    else {
+      this.updateBook(this.currentBookId, books);
+    }
   }
 
   onBookEdit(id:string) 
   {
-    console.log(this.form);
-    
+    let current = this.allBooks.find((b) => {return b.id === id});
+    this.currentBookId = id;
+
+    this.form.setValue({
+      title: current.title, 
+      author: current.author,
+      genre: current.genre,
+      language: current.language,
+      description: current.description,
+      coverImageFile: current.coverImageFile,
+      isAvailable: current.isAvailable,
+      isPremium: current.isPremium,
+      rentCount: current.rentCount,
+      id: current.id
+    });
+
+    this.editMode = true;
   }
 
   onBookDelete(id: string) {
@@ -54,6 +66,38 @@ export class AdminOptionsComponent implements OnInit {
       console.log(books); 
       this.allBooks = books;
     });
+  }
+
+  private createBook(books: {title: string, author: string, genre: string, language:string, description: string, 
+    coverImageFile: string, isAvailable: boolean, isPremium: boolean, rentCount: number, id: string}) {
+
+      if (books.isPremium != true){
+        books.isPremium = false;
+      }
+      if (books.isAvailable != true) {
+        books.isAvailable = false;
+      }
+
+      console.log(books);
+      this.service.createBook(books).subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  private updateBook(id: string, books: {title: string, author: string, genre: string, language:string, description: string, 
+    coverImageFile: string, isAvailable: boolean, isPremium: boolean, rentCount: number, id: string})
+  {
+
+    if (books.isPremium != true){
+      books.isPremium = false;
+    }
+    if (books.isAvailable != true) {
+      books.isAvailable = false;
+    }
+    
+    this.service.updateBook(id, books).subscribe((res) => {
+      console.log(res);
+    })
   }
 
   private deleteBook(id) {

d0e724fe2ac6f8e330446f954a0b7f30fda8f0b5
Ana Petrovic <pana.petrovic@gmail.com>
2022-08-17 12:15:24 +0200

    Dodat modul za cart

--- E-library/WebApp/LibrarySPA/src/app/app-routing.module.ts
+++ E-library/WebApp/LibrarySPA/src/app/app-routing.module.ts
@@ -6,8 +6,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 const appRoute: Routes = [
   {path: '', redirectTo: 'Home', pathMatch: 'full'},
   {path: 'Home', component: HomepageComponent},
+<<<<<<< Updated upstream
   {path: 'catalog', loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule)}, // catalog
   {path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) }
+=======
+  {path: 'catalog', loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule)},
+  { path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) },
+  { path: 'library', loadChildren: () => import('./library/library.module').then(m=>m.LibraryModule) },
+  {path: 'shopping-cart', loadChildren: () => import('./shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)}
+>>>>>>> Stashed changes
 ]
 
 @NgModule({

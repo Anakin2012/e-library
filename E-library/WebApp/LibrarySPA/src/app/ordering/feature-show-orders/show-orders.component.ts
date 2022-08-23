00ecb6001a295c6123f0dace817a77dfbab3cce4
Ana Petrovic <pana.petrovic@gmail.com>
2022-08-23 20:40:46 +0200

    izmene u order komponenti

--- E-library/WebApp/LibrarySPA/src/app/ordering/feature-show-orders/show-orders.component.ts
+++ E-library/WebApp/LibrarySPA/src/app/ordering/feature-show-orders/show-orders.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { IOrder } from '../domain/models/IOrder';
 import { OrderingFacadeService } from '../domain/app-services/ordering-facade.service';
 import { DatePipe } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, switchMap, take } from 'rxjs';
 
 
 @Component({
@@ -12,24 +13,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShowOrdersComponent implements OnInit {
 
-    username = '';
+    username: string = '';
     RouteParamObs;
     orders: IOrder[];
-    constructor(private activatedRoute: ActivatedRoute, public datepipe: DatePipe, private service: OrderingFacadeService) { }
+    constructor(private activatedRoute: ActivatedRoute, 
+                public datepipe: DatePipe,
+                private service: OrderingFacadeService)
+    { }
 
     ngOnInit(): void {
-
-        this.RouteParamObs = this.activatedRoute.paramMap.subscribe((param) => {
-            this.username = param.get('username');
-            this.getOrders(this.username);
-        });
+        this.getOrders();        
     }
 
-    private getOrders(username: string) {
-        this.service.getOrders(username).subscribe((orders) => {
+    private getOrders() {
+        this.RouteParamObs = this.activatedRoute.paramMap.pipe(
+            take(1),
+            map((paramMap: ParamMap) => {
+                this.username = paramMap.get('username');
+                return this.username;
+            }),
+            switchMap((username: string) => this.service.getOrders(username))
+        ).subscribe((orders) =>  {
             this.orders = orders;
             console.log(this.orders);
-        });
+        })
     }
 
 }

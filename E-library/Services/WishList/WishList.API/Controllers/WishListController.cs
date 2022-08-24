49fa846839d62c30b84f5eb07d4e5321cd50012c
altgmbobo <bbbole999@gmail.com>
2022-08-24 22:07:39 +0200

    Revert "Autorizacija u backendu"
    This reverts commit fade4f1a7354b68f46fca2cbc1b9f249d469b954.

--- E-library/Services/WishList/WishList.API/Controllers/WishListController.cs
+++ E-library/Services/WishList/WishList.API/Controllers/WishListController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using WishList.API.Entities;
 using WishList.API.Repositories;
@@ -25,44 +24,33 @@ namespace WishList.API.Controllers
             _service = service ?? throw new ArgumentNullException(nameof(repository));
         }
 
-        [Authorize(Roles = "Member,PremiumMember")]
+       // [Authorize(Roles = "Member")]
         [HttpGet("[action]/{username}")]
         [ProducesResponseType(typeof(WishBookList), StatusCodes.Status200OK)]
         public async Task<ActionResult<WishBookList>> GetList(string username) { 
-             if (User.FindFirst(ClaimTypes.Name).Value != username)
-            {
-                return Forbid();
-            }
             var basket = await _repository.GetList(username);
             return Ok(basket ?? new WishBookList(username));
         }
 
-        [Authorize(Roles = "Member,PremiumMember")]
+      //  [Authorize(Roles = "PremiumMember")]
         [HttpGet("recommendByAuthor/{username}")]
         [ProducesResponseType(typeof(WishBookList), StatusCodes.Status200OK)]
         public async Task<ActionResult<List<ListItem>>> GetRecommendationsByAuthor(string username)
         {
-            if (User.FindFirst(ClaimTypes.Name).Value != username)
-            {
-                return Forbid();
-            }
             var recommendations = await _service.getRecommendationsByAuthor(username);
             return Ok(recommendations);
         }
 
-        [Authorize(Roles="Member,PremiumMember")]
+     //   [Authorize(Roles="Member")]
         [HttpPut("/addBookToWishList/{username}/{bookId}")]
         [ProducesResponseType(typeof(WishBookList), StatusCodes.Status200OK)]
         public async Task<ActionResult<WishBookList>> AddToWishList(string username, string bookId)
         {
-            if (User.FindFirst(ClaimTypes.Name).Value != username)
-            {
-                return Forbid();
-            }
+
             return Ok(await _service.addBookToWishList(username, bookId));
         }
 
-        [Authorize(Roles = "Member,PremiumMember")]
+     //   [Authorize(Roles = "PremiumMember")]
         [HttpGet("recommendByGenre/{username}")]
         [ProducesResponseType(typeof(WishBookList), StatusCodes.Status200OK)]
         public async Task<ActionResult<List<ListItem>>> GetRecommendationsByGenre(string username)
@@ -78,7 +66,7 @@ namespace WishList.API.Controllers
         {
             return Ok(await _repository.UpdateList(list));
         }
-        [Authorize(Roles = "Member,PremiumMember")]
+
         [HttpDelete("{username}")]
         [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
         public async Task<IActionResult> DeleteList(string username)
@@ -87,16 +75,11 @@ namespace WishList.API.Controllers
             return Ok();
         }
 
-        [Authorize(Roles = "Member,PremiumMember")]
         [Route("[action]/{username}/{bookId}")]
         [HttpPut]
         [ProducesResponseType(typeof(WishBookList), StatusCodes.Status200OK)]
         public async Task<ActionResult<WishBookList>> RemoveItemFromWishlist(string username, string bookId)
         {
-            if (User.FindFirst(ClaimTypes.Name).Value != username)
-            {
-                return Forbid();
-            }
             return Ok(await _repository.RemoveItemFromWishlist(username, bookId));
         }
     }

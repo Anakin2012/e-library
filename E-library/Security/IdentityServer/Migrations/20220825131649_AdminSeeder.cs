0ddb4b0b6b9b54961db78193c72ce6da79aa0642
josicb16 <95657159+josicb16@users.noreply.github.com>
2022-08-25 15:20:45 +0200

    Napravljen seeder za administratora

---
+++ E-library/Security/IdentityServer/Migrations/20220825131649_AdminSeeder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Migrations
+{
+    public partial class AdminSeeder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "3be795b3-981d-4271-92e0-5d57d2801df3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "e34527ba-5ad5-41ce-b979-900445ce5393");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "6c607bcc-9929-4bad-8d6b-0172d35eb79f");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Credentials", "DateMembership", "Email", "EmailConfirmed", "IsMembershipPaid", "LockoutEnabled", "LockoutEnd", "Name", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Surname", "TwoFactorEnabled", "UserName" },
+                values: new object[] { 1, 0, "6af00783-98e5-44d6-93d2-1e21abdbd1b8", 0.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "napoleon77@ethereal.email", false, true, false, null, "Admin", "NAPOLEON77@ETHEREAL.EMAIL", "ADMIN", "AQAAAAEAACcQAAAAELbU8HfDkG8+zpv0q+0dnZjCU0vtMGfsiCeDO+Np4xN4C1yh4iIZmEIrK3C4uq8Eaw==", null, false, null, "Admin", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { 1, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "f522f25f-c1dd-4870-aede-c684e9260a8c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d6e581d3-cf46-4378-adbd-09e6ff7288e8");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "5d978f9c-dbb6-454e-b2de-7dc74772ac69");
+        }
+    }
+}
